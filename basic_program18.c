02bc90145b454cdcf9007a564a75f36eb09983e3
SAGAR T <ssagu6661@gmail.com>
2024-10-01 13:20:28 +0530

    Add files via upload

---
+++ basic_program18.c
@@ -0,0 +1,32 @@
+// #include<stdio.h>
+// void main(){
+// int i=1;
+// do
+// {
+//    printf("Hello World \n",  i++);
+// } while (i<11);
+// {
+// }
+// }
+
+
+
+// #include<stdio.h>
+// void main(){
+// int i=1;
+// while (i<11)
+// {
+//    printf("Hello World \n",  i++);
+// }
+
+// }
+
+#include<stdio.h>
+void main(){
+int i;
+printf("9 times of hello world\n");
+for (int i = 0; i <= 10;i++ )
+{
+  printf("Hello world : %d\n",i);
+}
+}
